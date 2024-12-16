815c84e8e89043bc5f269eb40df2c95824a673d0
Belkayou07 <belkayou07@gmail.com>
2024-12-16 12:25:34 +0100

    GOOGLE MAPS UPDATE

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -156,7 +156,7 @@ export const ContactSection: React.FC<{ scrollToSection?: (section: string) => v
               <iframe 
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2515.066595734172!2d4.512151076309904!3d50.92248507168606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3ddfed5244c6d%3A0xec5df86b1ee8fb64!2sLesagestraat%2039%2C%201820%20Steenokkerzeel!5e0!3m2!1snl!2sbe!4v1734314540192!5m2!1snl!2sbe" 
                 width="100%" 
-                height="300" 
+                height="100%" 
                 className="w-full"
               ></iframe>
             </MapImage>
